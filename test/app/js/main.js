2c8018d76e4e3fa7b20fe1b0f09c37c5da2ac8c9
Ilya Govorukhin <Govoruxin3@mail.ru>
2015-11-13 01:34:43 +0300

    end

---
+++ test/app/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    document.getElementById('check1').addEventListener('change', function(event) {
+        document.getElementById('check2').classList.toggle("foo");
+    });
+
+
+
+})
